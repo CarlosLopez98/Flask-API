dd364a11dec56b10612632062c2544bb7cae995a
CarlosLopez98 <carlos980718@gmail.com>
2020-08-08 19:25:54 -0500

    Put request

--- test.py
+++ test.py
@@ -2,5 +2,8 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-response = requests.get(BASE + "helloworld/carlos")
+#response = requests.put(BASE + "video/1", {"name": "Video 1", "views": 100, "likes": 10})
+#print(response.json())
+
+response = requests.get(BASE + "video/6")
 print(response.json())
