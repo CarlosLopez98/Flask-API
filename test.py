97ccd18c34ca0b31839565a063327c03355d9e97
CarlosLopez98 <carlos980718@gmail.com>
2020-08-08 20:39:30 -0500

    CRUD complete

--- test.py
+++ test.py
@@ -2,8 +2,21 @@ import requests
 
 BASE = "http://localhost:5000/"
 
-#response = requests.put(BASE + "video/1", {"name": "Video 1", "views": 100, "likes": 10})
-#print(response.json())
+data = [
+	{"name": "Video 1", "views": 100, "likes": 10},
+	{"name": "Video 2", "views": 1200, "likes": 150},
+	{"name": "Video 3", "views": 300, "likes": 20}
+]
 
-response = requests.get(BASE + "video/6")
+for i in range(len(data)):
+	response = requests.put(BASE + "video/" + str(i), data[i])
+	print(response.json())
+
+response = requests.get(BASE + "video/2")
 print(response.json())
+
+response = requests.patch(BASE + "video/2", {"name": "Video nuevo 3"})
+print(response.json())
+
+response = requests.delete(BASE + "video/2")
+print(response)
